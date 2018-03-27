e8a95c83fdeba68417737755711443e8ade3fb31
Greg Wilson <greg.wilson5@outlook.com>
2018-03-27 10:57:42 -0400

    solved paginate test

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -63,9 +63,9 @@ namespace SportsStore.UnitTests
             MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
 
             //Assert
-            Assert.AreEqual(@"<a class=""btn btn-default""href=""Page1"">1</a>"
-         + @"<a class=""btn btn-default btn-primary selected""href=""Page2"">2</a>"
-         + @"<a class=""btn btn-default""href=""Page3"">3</a>", result.ToString());
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+         + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
+         + @"<a class=""btn btn-default"" href=""Page3"">3</a>", result.ToString());
         }
 
         [TestMethod]
